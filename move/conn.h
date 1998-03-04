b458aa073c73e284a47e20ce9f64d0fd4b7b3faa
tonyg <devnull@localhost>
1998-03-04 02:31:28 +0000

    [project @ 1998-03-04 02:31:28 by tonyg] Altered conn_closed macro to include checking for NULL. Don't know how useful this is.

--- move/conn.h
+++ move/conn.h
@@ -13,7 +13,7 @@ extern int conn_write(const char *buf, int size, OVECTOR conn);
 extern int conn_puts(const char *s, OVECTOR conn);
 
 extern void conn_close(OVECTOR conn);
-#define conn_closed(conn)	(NUM(AT(conn, CO_TYPE)) == CONN_NONE)
+#define conn_closed(conn)	((conn) == NULL || NUM(AT(conn, CO_TYPE)) == CONN_NONE)
 
 extern void fill_scaninst(SCANINST si, OVECTOR conn);
 
